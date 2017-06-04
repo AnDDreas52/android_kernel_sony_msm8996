bf1e438e2dafa0306f51c7bb0227e508205576e4
AnDDreas52 <anddreas52@gmail.com>
2017-06-04 13:18:49 +0200

    Revert "Decrease BT/FM TX timeouts to improve deep sleep ratio."
    This reverts commit 6bb3f9b8cd7f6c2f2a0bfb6921b148d56f3e97c2.

--- drivers/bluetooth/bluesleep.c
+++ drivers/bluetooth/bluesleep.c
@@ -120,8 +120,8 @@ DECLARE_DELAYED_WORK(sleep_workqueue, bluesleep_sleep_work);
 #define bluesleep_rx_idle()     schedule_delayed_work(&sleep_workqueue, 0)
 #define bluesleep_tx_idle()     schedule_delayed_work(&sleep_workqueue, 0)
 
-/* 1 second timeout */
-#define TX_TIMER_INTERVAL 1
+/* 5 second timeout */
+#define TX_TIMER_INTERVAL  5
 
 /* state variable names and bit positions */
 #define BT_PROTO	0x01
